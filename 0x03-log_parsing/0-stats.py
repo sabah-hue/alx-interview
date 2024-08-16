467c9eeb3d5617e3f5995e06c696c74155021cfc
sabah-hue <sabah.abdelbaset@gmail.com>
2024-08-16 05:35:31 +0300

    log

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" script that reads stdin line by line and computes metrics """
+import sys
+
+
+status_dict = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+size = 0
+count = 0
+try:
+    for line in sys.stdin:
+        data = line.split()
+        count += 1
+        try:
+            if len(data) >= 5 and data[-2].isdigit() and data[-1].isdigit():
+                status_code = int(data[-2])
+                if status_code in status_dict:
+                    status_dict[status_code] += 1
+        except BaseException:
+            pass
+
+        try:
+            if len(data) >= 6 and data[-1].isdigit():
+                size += int(data[-1])
+        except BaseException:
+            pass
+
+        if count % 10 == 0 or sys.stdin.closed:
+            print(f"File size: {size}")
+            for status_code in sorted(status_dict.keys()):
+                if status_dict[status_code]:
+                    print(f"{status_code}: {status_dict[status_code]}")
+
+except KeyboardInterrupt:
+    print(f"File size: {size}")
+    for status_code in sorted(status_dict.keys()):
+        if status_dict[status_code]:
+            print(f"{status_code}: {status_dict[status_code]}")
+    raise
