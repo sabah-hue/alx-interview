eb432dc7b640e467b00706b048cbe6b562f9a6bc
sabah-hue <sabah.abdelbaset@gmail.com>
2024-08-29 18:24:11 +0300

    add back

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,6 +1,35 @@
 #!/usr/bin/python3
 """ N queens puzzle """
 import sys
+import random
+
+
+def back(r, n, cols, pos, neg, board):
+    """ track queens solutions """
+    if r == n:
+        res = []
+        for l in range(len(board)):
+            for k in range(len(board[l])):
+                if board[l][k] == 1:
+                    res.append([l, k])
+        print(res)
+        return
+
+    for col in range(n):
+        if col in cols or (r + col) in pos or (r - col) in neg:
+            continue
+
+        cols.add(col)
+        pos.add(r + col)
+        neg.add(r - col)
+        board[r][col] = 1
+
+        back(r+1, n, cols, pos, neg, board)
+
+        cols.remove(col)
+        pos.remove(r + col)
+        neg.remove(r - col)
+        board[r][col] = 0
 
 
 if len(sys.argv) != 2:
@@ -14,16 +43,9 @@ except ValueError:
 if n < 4:
     print("N must be at least 4")
     sys.exit(1)
-for i in range(n - 2):
-    x = []
-    for j in range(n):
-        if j == 0:
-            z = i + 1
-        elif j == n - 1:
-            z = n - 2 - i
-        elif j > i:
-            z = j - i
-        else:
-            z = i - j
-        x.append([j, z])
-    print(f"{x}")
+
+colums = set()
+pos = set()
+neg = set()
+board = [[0] * n for i in range(n)]
+back(0, n, colums, pos, neg, board)
