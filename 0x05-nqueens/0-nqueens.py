498971bc4674ae655c4de5a027f0315a13591c96
sabah-hue <sabah.abdelbaset@gmail.com>
2024-08-29 08:51:06 +0300

    handle esp case

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" N queens puzzle """
+import sys
+
+
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+try:
+    n = int(sys.argv[1])
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
+if n < 4:
+    print("N must be at least 4")
+    sys.exit(1)
