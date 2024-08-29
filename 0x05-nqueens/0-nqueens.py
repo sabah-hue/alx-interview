9304964b05a2d3cc3f95f9c7b0cb9226999c6a2d
sabah-hue <sabah.abdelbaset@gmail.com>
2024-08-29 09:34:37 +0300

    debug

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -14,3 +14,16 @@ except ValueError:
 if n < 4:
     print("N must be at least 4")
     sys.exit(1)
+for i in range(n - 2):
+    x = []
+    for j in range(n):
+        if j == 0:
+            z = i + 1
+        elif j == n - 1:
+            z = n - 2 - i
+        elif j > i:
+            z = j - i
+        else:
+            z = i - j
+        x.append([j, z])
+    print(f"{x}")
