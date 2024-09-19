309cc26cc221982756d5e0853dc9214fe2c8583d
sabah-hue <sabah.abdelbaset@gmail.com>
2024-09-19 07:29:12 +0300

    coins

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Change comes from within """
+
+
+def makeChange(coins, total):
+    """
+    determine the fewest number of coins
+    needed to meet a given i total
+    """
+    if total <= 0:
+        return 0
+    
+    dp = [float('inf')] * (total + 1)
+    
+    dp[0] = 0
+    
+    for i in range(1, total + 1):
+        max_coin = max(coins)
+        
+        if i == max_coin:
+            dp[i] = 1
+        else:
+            for coin in coins:
+                if coin <= i:
+                    dp[i] = min(dp[i], 
+                                      dp[i - coin] + 1)
+    
+    return dp[total] if dp[total] != float('inf') else -1
