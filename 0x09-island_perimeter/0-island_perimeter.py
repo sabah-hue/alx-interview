39650cf8e5047064a5e61a28ec1002b7a3a5313b
sabah-hue <sabah.abdelbaset@gmail.com>
2024-09-25 16:04:22 +0300

    pycodestyle

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -16,14 +16,10 @@ def island_perimeter(grid):
             if grid[row][col] == 1:
                 if row == 0 or grid[row-1][col] == 0:
                     perimeter += 1
-                
                 if row == rows - 1 or grid[row+1][col] == 0:
                     perimeter += 1
-                
                 if col == 0 or grid[row][col-1] == 0:
                     perimeter += 1
-                
                 if col == cols - 1 or grid[row][col+1] == 0:
                     perimeter += 1
-    
     return perimeter
